4385a7975ae15bbe00638de8b6063d392abd5984
Tyler Maatrix <tylermaatrix@gmail.com>
2021-02-04 10:00:11 -0500

    added and styled navbar

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 
 setInterval(() => {
   i++;
-
   const icon = document.querySelector(".section-1-icons .change");
-
   icon.classList.remove("change");
 
   if (i > icons.length) {
@@ -14,4 +18,4 @@ setInterval(() => {
   } else {
     icon.nextElementSibling.classList.add("change");
   }
-}, 3000);
+}, 4000);
